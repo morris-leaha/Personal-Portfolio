dd15a7b7ba2e64ec2796f75efa5127605a89af0f
LAMorris <morris.leaha@gmail.com>
2019-05-23 15:58:18 -0500

    [public/javascript/app.js] commented out contact form | moved functions around

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -23,60 +23,60 @@ $(document).ready(function () {
     } // End if
   });
 
-  $(document).on("click", ".submit-btn", insertContact);
-
-
-  function insertContact(event) {
-    event.preventDefault();
-    var contact = {
-      name: $("#name").val().trim(),
-      email: $("#email").val().trim(),
-      subject: $("#subject").val().trim(),
-      textArea: $("#textArea").val().trim()
-    };
-    console.log(contact);
-    $.ajax({
-      url: "/api/contact",
-      type: "POST",
-      data: contact,
-    });
-    window.location.href = "/index";
+  // When the user scrolls the page, execute myFunction 
+  // adapted from W3 Schools at: https://www.w3schools.com/howto/howto_js_sticky_header.asp
+  window.onscroll = function () {
+    navbarSticky();
+    scrollFunction();
   };
+
+  // Get the header
+  var navbar = document.getElementById("nav");
+
+  // Get the offset position of the navbar
+  var sticky = navbar.offsetTop;
+
+  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function navbarSticky() {
+    if (window.pageYOffset > sticky) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
+      document.getElementById("topBtn").style.display = "block";
+    } else {
+      document.getElementById("topBtn").style.display = "none";
+    }
+  }
+
+  // $(document).on("click", ".submit-btn", insertContact);
+
+
+  // function insertContact(event) {
+  //   event.preventDefault();
+  //   var contact = {
+  //     name: $("#name").val().trim(),
+  //     email: $("#email").val().trim(),
+  //     subject: $("#subject").val().trim(),
+  //     textArea: $("#textArea").val().trim()
+  //   };
+  //   console.log(contact);
+  //   $.ajax({
+  //     url: "/api/contact",
+  //     type: "POST",
+  //     data: contact,
+  //   });
+  //   window.location.href = "/index";
+  // };
 
 });
 
-// When the user scrolls the page, execute myFunction 
-// adapted from W3 Schools at: https://www.w3schools.com/howto/howto_js_sticky_header.asp
-window.onscroll = function () {
-  navbarSticky();
-  scrollFunction();
-};
-
-// Get the header
-var navbar = document.getElementById("nav");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function navbarSticky() {
-  if (window.pageYOffset > sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
-function scrollFunction() {
-  if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
-    document.getElementById("topBtn").style.display = "block";
-  } else {
-    document.getElementById("topBtn").style.display = "none";
-  }
-}
-
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
